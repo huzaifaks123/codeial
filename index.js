d6be4624fcc72a514930ccc8c12f56ec16539701
huzaifaks123 <huzaifaks123@gmail.com>
2024-02-18 16:28:00 +0530

    setup our static files

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-const router = 
-app.use('/', require('./routes'))
+const expressLayouts = require('express-ejs-layouts')
+
+app.use(express.static('./assets'))
+
+app.use(expressLayouts)
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+const router = app.use('/', require('./routes'))
 
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', './views');
 
 app.listen(port,function(err){
     if(err){
